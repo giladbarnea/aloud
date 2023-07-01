f82122e699f7793feacdc1b576a9b54508db7e45
Gilad Barnea <gilad.b@pecan.ai>
2023-07-01 09:44:00 +0300

    main.py

--- aloud/main.py
+++ aloud/main.py
@@ -1,9 +1,18 @@
 from pathlib import Path
+import elevenlabs as xi
+from langchain.document_loaders import WebBaseLoader
 
-from elevenlabs import generate, play, set_api_key
 
-set_api_key(Path("~/.elevenlabs-token").expanduser().read_text().strip())
+def main():
+    xi.set_api_key(Path("~/.elevenlabs-token").expanduser().read_text().strip())
+    url = "https://www.deconstructconf.com/blog/how-to-prepare-a-talk"
+    loader = WebBaseLoader(url)
+    data = loader.load()
+    print(data)
+    # audio = xi.generate(text="Hi! My name is Bella, nice to meet you!", voice="Bella", model="eleven_monolingual_v1")
 
-audio = generate(text="Hi! My name is Bella, nice to meet you!", voice="Bella", model="eleven_monolingual_v1")
+    # xi.play(audio)
 
-play(audio)
+
+if __name__ == "__main__":
+    main()
