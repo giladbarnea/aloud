d97ac751e51bd4fb706dbcbfc0b6386bf41ac0a0
Gilad Barnea <gilad.b@pecan.ai>
2023-11-23 11:45:03 +0200

    change prompt for more varied transitions

--- aloud/convert/to_speakable.py
+++ aloud/convert/to_speakable.py
@@ -23,11 +23,12 @@ TO_SPEAKABLE: Prompt = """
 You are given a markdown representation of an article from the internet.
 
 Convert the syntax of the markdown into text that can be read out, and keep any real text that is not markdown syntax as-is.
-The general principle is that, as you know, saying "hashtag hastag <something>" does not make sense to humans, so you should convert that to something like "Moving on to the next part: <something>.", or "Next: <something>", "Next Part: <something>", etc (You can make up new ways to say this. Be creative, mix it up).
+The general principle is that, as you know, saying "hashtag hastag <something>" does not make sense to humans, so you should convert that to something like "Moving on to the next part: <something>.", "Next: <something>", "Next Part: <something>", "Next Section: <something>", etc. You can make up new ways to say this. Be creative, mix it up.
 Similarly, saying "Open square brackets, Press here, close square brackets, open parenthesis, https://www.google.com, close parenthesis" does not make sense to humans, so you should convert that to "There's a link to Google here.".
 If a title is followed immediately by a subtitle, say "Moving on to the next part: <title>. Subtitle: <subtitle>".
+Be mindful of how many levels of markdown headers the article contains; Choose the most appropriate way to announce the header, based on its depth. Different depths need to be announced differently.
 If you encounter a table, replace it with "There's a table here that communicates the following information: <information>", where <information> is a short textual summary of the data in the table, and any conclusions that the reader is supposed to draw from it.
-If you encounter code, replace it with "There's a code block here that <description>", where <description> is a short textual description of what the code accepts as input, what it outputs and what takeaway the reader was supposed to get from it.
+If you encounter code, replace it with "There's a code block here that <description>", where <description> is a short textual description of what the code accepts as input, what it outputs and what takeaway the reader was supposed to get from it. If there are a few code blocks somewhat consequtively, announce the subsequent ones with "Another code block describes <description>", "An additional code block shows <description>", etc.
 Sentences that are completely in emphasis should be announced as "Now, this is important: <sentence>", or "This is important: <sentence>", or "Note, the following is emphasized: <sentence>", or "The following is emphasized: <sentence>", etc (You can make up new ways to say this. Be creative, mix it up).
 Sentences with a word or a short phrase in emphasis should be followed by "the '<emphasized-words-with-hyphens>' part was emphasized".
 Generalize this to the entirety of the markdown syntax.
@@ -79,7 +80,7 @@ def to_speakable(thing: str | Path, output_dir: str | Path) -> Generator[str, No
             speakable += delta
             speakable_lines = speakable.splitlines()
             display_speakable = Text()
-            num_lines = console.height
+            num_lines = console.height - 5
             for i, line in enumerate(speakable_lines[-num_lines:]):
                 color_rgb = get_gradient_color(start_color, end_color, num_lines - 1, i)
                 color = Color.from_rgb(*color_rgb)
