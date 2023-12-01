b71bdcc9991cfa1dd076709006b4809e965e46f9
Gilad Barnea <gilad.b@pecan.ai>
2023-12-01 14:10:08 +0200

    reshaping-the-tree-rebuilding-organizations test case

---
+++ tests/links_and_images/reshaping-the-tree-rebuilding-organizations/reshaping-the-tree-rebuilding-organizations.py
@@ -0,0 +1,6 @@
+def test_reshaping_the_tree_rebuilding_organizations():
+    article_url = "https://www.oneusefulthing.org/p/reshaping-the-tree-rebuilding-organizations"
+    from aloud.main import process
+
+    speakable: bytes = process(article_url, only_speakable=True)
+    audio = process(speakable.decode(), only_audio=True)
